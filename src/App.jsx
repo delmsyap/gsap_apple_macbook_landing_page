92113458751bcd9fb67700ca7c56c1e6baa2a80d
delmsyap <yapdelmar@gmail.com>
2026-02-09 16:24:52 +0000

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
        <main>
            <NavBar />
+           <Hero />
        </main>
     )
 }
